2f2bf67f4e3792c6538d0cbf5fbb1cf2ecc21d8f
livelivelive007 <jeremylive096@gmail.com>
2020-10-13 17:02:57 -0600

    Revert "Revert "last update :9""
    This reverts commit 735221f43fddd8c01e24c93d0e866bd5f98bd233.

--- Tablero.java
+++ Tablero.java
@@ -115,9 +115,9 @@ class Tablero
 				jugada.puntos+=contarFila(jugadita);			
 		}
 		if(jugada.isLine==null||jugada.isLine.booleanValue())
-			jugada.puntos+=contarFila(jugada.jugaditas.get(0));
+			jugada.puntos+=contarFila(jugada.jugaditas.iterator().next());
 		else
-			jugada.puntos+=contarColumna(jugada.jugaditas.get(0));
+			jugada.puntos+=contarColumna(jugada.jugaditas.iterator().next());
 		return jugada.puntos;
 	}
 	private int contarColumna(Jugadita jugadita){
